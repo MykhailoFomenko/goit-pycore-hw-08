b0297f94b93be03b1e41b34ad33a9b4a49e11795
MykhailoFomenko <mishafomenko1505@gmail.com>
2024-07-19 09:12:17 +0200

    Update goit-pycore-hw-08.py

--- goit-pycore-hw-08.py
+++ goit-pycore-hw-08.py
@@ -90,6 +90,7 @@ class AddressBook(UserDict):
                     contact.add_birthday(datetime.strftime(i.get("birthday"), "%d.%m.%Y"))  
                  else:
                     continue
+            self.data.update({contact.name.value : contact.phones}) 
             return contact
         else:
             return None
@@ -144,7 +145,6 @@ def add_contact(args, book: AddressBook):
         message = "Contact added."
     if phone:
         record.add_phone(phone)
-        book.add_record(record)
     return message
 
 @input_error
